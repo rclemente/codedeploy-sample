090a4f6ee013878455d5711cecda05b0f9a957bc
ryan clemente <rclemente@cafepress.com>
2015-06-30 13:04:50 +0800

    change to plain copy for simplicity

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 exports.handler = function(event, context) {
   var message = event.Records[0].Sns.Message
   console.log('message= '+ message);
-  context.succeed('finished');
+  context.succeed('finished extractUtmParams');
 };
